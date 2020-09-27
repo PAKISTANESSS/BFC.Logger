8024904f258312a0888868ad0f6bf62fa64cfa4d
Bernardo Coelho <fcoelhobernardo@gmail.com>
2020-09-27 18:09:26 +0100

    Update LoggerConfigurationTest.cs

--- src/BFC.Logger.Test/LoggerConfigurationTest.cs
+++ src/BFC.Logger.Test/LoggerConfigurationTest.cs
@@ -7,11 +7,27 @@ namespace BFC.Logger.Test
     public class LoggerConfigurationTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void LoggerConfiguration_SetupCorrectly_Database_Test()
         {
             var target = LoggerConfiguration.Setup(LogTarget.Database);
 
             Assert.AreEqual(target, LogTarget.Database);
+        }
+
+        [TestMethod]
+        public void LoggerConfiguration_SetupCorrectly_File_Test()
+        {
+            var target = LoggerConfiguration.Setup(LogTarget.File);
+
+            Assert.AreEqual(target, LogTarget.File);
+        }
+
+        [TestMethod]
+        public void LoggerConfiguration_SetupWithDifferentTargets_Test()
+        {
+            var target = LoggerConfiguration.Setup(LogTarget.File);
+
+            Assert.AreNotEqual(target, LogTarget.Database);
         }
     }
 }
